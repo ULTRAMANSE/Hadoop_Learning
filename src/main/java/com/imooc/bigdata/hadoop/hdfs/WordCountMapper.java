864101af68b394aba7cff8991e737c5cc2a9faeb
Zhang-Yixuan <1428431375@qq.com>
2019-03-10 17:53:10 +0800

    HDFS API开发：自定义实现词频统计算法

---
+++ src/main/java/com/imooc/bigdata/hadoop/hdfs/WordCountMapper.java
@@ -0,0 +1,22 @@
+package com.imooc.bigdata.hadoop.hdfs;
+
+/**
+ * 自定义wc实现类
+ */
+public class WordCountMapper implements ImoocMapper {
+
+    public void map(String line, ImoocContext context) {
+        String[] words = line.split(" ");
+
+        for(String word : words) {
+            Object value = context.get(word);
+            if(value == null) { // 表示没有出现过该单词
+                context.write(word, 1);
+            } else {
+                int v = Integer.parseInt(value.toString());
+                context.write(word, v+1);  // 取出单词对应的次数+1
+            }
+        }
+
+    }
+}

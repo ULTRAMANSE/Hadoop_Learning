681020ebd670a906267e493262553bff56df6b7b
Zhang-Yixuan <1428431375@qq.com>
2019-03-10 17:49:35 +0800

    HDFS API开发：将仓库中的信息进行引入

---
+++ src/main/java/com/imooc/bigdata/hadoop/hdfs/Constants.java
@@ -0,0 +1,16 @@
+package com.imooc.bigdata.hadoop.hdfs;
+
+public class Constants {
+
+    public static final String INPUT_PATH = "INPUT_PATH";
+
+    public static final String OUTPUT_PATH = "OUTPUT_PATH";
+
+    public static final String OUTPUT_FILE = "OUTPUT_FILE";
+
+    public static final String HDFS_URI = "HDFS_URI";
+
+    public static final String MAPPER_CLASS = "MAPPER_CLASS";
+
+
+}

dfb212e9a16a94cbd1fc77590a8cddb2d3de95a6
Zhang-Yixuan <1428431375@qq.com>
2019-03-25 18:55:48 +0800

    日志解析

--- src/main/java/com/immoc/bigdata/hadoop/mr/project/utils/LogParser.java
+++ src/main/java/com/immoc/bigdata/hadoop/mr/project/utils/LogParser.java
@@ -1,41 +1,79 @@
 package com.immoc.bigdata.hadoop.mr.project.utils;
 
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class LogParser {
 
-    public Map<String , String>parse(String log){
-
-        Map<String,String>info = new HashMap<>();
-        IPParser ipParser = IPParser.getInstance();
-
-        if(StringUtils.isNotBlank(log)){
-            String[] splits = log.split("\001");
-            String ip = splits[13];
-            String country = " ";
-            String province = " ";
-            String city = " ";
-
-            IPParser.RegionInfo regionInfo = ipParser.analyseIp(ip);
-
-            if(regionInfo != null){
-                country = regionInfo.getCountry();
-                province = regionInfo.getCountry();
-                city = regionInfo.getCity();
-            }
-
-            info.put("ip",ip);
-            info.put("country",country);
-            info.put("province",province);
-            info.put("city",city);
+    private Logger logger = LoggerFactory.getLogger(LogParser.class);
 
 
+
+    public Map<String, String> parse2(String log)  {
+
+        Map<String, String> logInfo = new HashMap<String,String>();
+        IPParser ipParse = IPParser.getInstance();
+        if(StringUtils.isNotBlank(log)) {
+            String[] splits = log.split("\t");
+
+            String ip = splits[0];
+            String url = splits[1];
+            String sessionId = splits[2];
+            String time = splits[3];
+            String country = splits[4];
+            String province = splits[5];
+            String city = splits[6];
+
+            logInfo.put("ip",ip);
+            logInfo.put("url",url);
+            logInfo.put("sessionId",sessionId);
+            logInfo.put("time",time);
+            logInfo.put("country",country);
+            logInfo.put("province",province);
+            logInfo.put("city",city);
+
+        } else{
+            logger.error("日志记录的格式不正确：" + log);
         }
-        
-        return info;
+
+        return logInfo;
+    }
+
+    public Map<String, String> parse(String log)  {
+
+        Map<String, String> logInfo = new HashMap<String,String>();
+        IPParser ipParse = IPParser.getInstance();
+        if(StringUtils.isNotBlank(log)) {
+            String[] splits = log.split("\001");
+
+            String ip = splits[13];
+            String url = splits[1];
+            String sessionId = splits[10];
+            String time = splits[17];
+
+            logInfo.put("ip",ip);
+            logInfo.put("url",url);
+            logInfo.put("sessionId",sessionId);
+            logInfo.put("time",time);
+
+
+            IPParser.RegionInfo regionInfo = ipParse.analyseIp(ip);
+
+            logInfo.put("country",regionInfo.getCountry());
+            logInfo.put("province",regionInfo.getProvince());
+            logInfo.put("city",regionInfo.getCity());
+
+        } else{
+            logger.error("日志记录的格式不正确：" + log);
+        }
+
+        return logInfo;
     }
 
 }
+
+
